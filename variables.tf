7ee0eb013fd2efba0ff95f02c2245591ad73ad01
gadom654 <dominikgadomski654@gmail.com>
2026-01-09 13:16:59 +0100

    Fix formatting of default values in variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
 variable "prefix" {
   type        = string
   description = "The prefix which should be used for all resources in this example"
-  default = "test"
+  default     = "test"
 }
 
 variable "location" {
   type        = string
   description = "The Azure Region in which all resources in this example should be created."
-  default = "westeurope"
+  default     = "westeurope"
 }
 
 variable "tags" {
